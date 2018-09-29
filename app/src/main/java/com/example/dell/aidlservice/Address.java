0da3da9ff05c523ed952892b5c8e2d83321f7543
yantao.pu <12345678>
2018-09-29 10:31:55 +0800

    本机Ip的获取

---
+++ app/src/main/java/com/example/dell/aidlservice/Address.java
@@ -0,0 +1,43 @@
+package com.example.dell.aidlservice;
+
+import java.lang.reflect.Array;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+public class Address {
+
+     public static void main(String[] rgc){
+         try {
+             InetAddress inetAddress = InetAddress.getLocalHost();
+             System.out.println(inetAddress.getAddress());
+             System.out.println(inetAddress.getHostAddress()); //IP地址
+             System.out.println(inetAddress.getHostName()); //本机名
+
+
+             System.out.println(Arrays.toString(inetAddress.getAddress())); //字节数组形式的IP地址
+             System.out.println(inetAddress.getCanonicalHostName());
+
+
+             System.out.println(inetAddress);
+
+         } catch (UnknownHostException e) {
+             e.printStackTrace();
+         }
+
+     }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
